c5c96e66661c3f20fe464813e480aad170e190e5
William Pettersson <wipe2400@student.miun.se>
2025-03-03 21:48:26 +0100

    Add map

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -3,6 +3,14 @@
 const searchBar = document.getElementById("search-bar");
 const searchBtn = document.getElementById("search-btn");
 
+// Initialize map and marker
+const map = L.map("map").setView([51.3406321, 12.3747329], 13);
+const marker = L.marker([51.3406321, 12.3747329]).addTo(map);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+}).addTo(map);
+
 /**
  * Get location data based on search term
  */
@@ -27,7 +35,31 @@ async function getData() {
 function getCoordinates(data) {
     const posLat = data[0].lat;
     const posLon = data[0].lon;
+    updateMap(posLon, posLat);
 }
 
-addEventListener("load", getData);
+/**
+ * Updates map position and marker
+ * @param {number} lon - New position longitude
+ * @param {number} lat - New position latitude
+ */
+function updateMap(lon, lat) {
+    map.panTo([lat, lon]);
+    marker.setLatLng([lat, lon]);
+}
+
+/**
+ * Checks if enter was pressed and calls "getData()"
+ * @param {string} key - Key that got pressed down
+ */
+function checkKey(key) {
+    if (key.code === "Enter") {
+        getData();
+        // Prevent enter from submitting form(which would reload the page)
+        key.preventDefault();
+        return false;
+    }
+}
+
 searchBtn.addEventListener("click", getData);
+searchBar.addEventListener("keydown", checkKey);
